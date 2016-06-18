2b13ad3681c4aa76d68d48a453b1cf2e8c695a15
xGioe <giovanni@mriva.me>
2016-06-18 22:36:18 +0200

    add reply form for admin with toggle animation in jquery

--- pages/restaurant_profile.js
+++ pages/restaurant_profile.js
@@ -1,8 +1,16 @@
 
 $(document).ready(function(){
   $('.ui.rating').rating();
-});
 
-$(document).ready(function(){
-    $('.tabular.menu .item').tab();
+  $('.tabular.menu .item').tab();
+
+  $('.reply.form').hide();
+
+  $('.actions .reply').click(function(){
+    $(this).parent().next().toggle();
+  });
+
+  $('.reply.form .button').click(function(){
+    $(this).parent().toggle();
+  });
 });
