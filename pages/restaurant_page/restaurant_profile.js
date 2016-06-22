68f377b9821d5e380e4e7af6dada1ef4277719e3
xGioe <giovanni@mriva.me>
2016-06-22 18:57:01 +0200

    code refactoring

--- pages/restaurant_page/restaurant_profile.js
+++ pages/restaurant_page/restaurant_profile.js
@@ -15,28 +15,25 @@ $(document).ready(function(){
   });
 
   // Image slider control functions
-  $(function(){
-    $("#slides").slidesjs({
-      width: 300,
-      height: 300,
-      play: {
-        active: false,
-          // [boolean] Generate the play and stop buttons.
-          // You cannot use your own buttons. Sorry.
-        effect: "slide",
-          // [string] Can be either "slide" or "fade".
-        interval: 2000,
-          // [number] Time spent on each slide in milliseconds.
-        auto: true,
-          // [boolean] Start playing the slideshow on load.
-        swap: true,
-          // [boolean] show/hide stop and play buttons
-        pauseOnHover: false,
-          // [boolean] pause a playing slideshow on hover
-        restartDelay: 2500
-          // [number] restart delay on inactive slideshow
-      }
-    });
+  $("#slides").slidesjs({
+    width: 800,
+    height: 800,
+    play: {
+      active: true,
+        // [boolean] Generate the play and stop buttons.
+        // You cannot use your own buttons. Sorry.
+      effect: "slide",
+        // [string] Can be either "slide" or "fade".
+      interval: 2000,
+        // [number] Time spent on each slide in milliseconds.
+      auto: false,
+        // [boolean] Start playing the slideshow on load.
+      swap: true,
+        // [boolean] show/hide stop and play buttons
+      pauseOnHover: false,
+        // [boolean] pause a playing slideshow on hover
+      restartDelay: 2500
+        // [number] restart delay on inactive slideshow
+    }
   });
-
 });
